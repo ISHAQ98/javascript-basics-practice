38ecee0065a1754583349fd44aa13a9a14e0cf28
ISHAQ <68194895+ISHAQ98@users.noreply.github.com>
2022-05-29 19:13:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -472,6 +472,27 @@ if (temp > 0 && temp < 30 && sunny !== false) {
   console.log("The weather is bad!");
 } */
 
+//====== Day 9 ========
+// EX1 ===========
+/*
+const countries = ["Finland", "Sweden", "Denmark", "Norway", "IceLand"];
+const names = ["Asabeneh", "Mathias", "Elias", "Brook"];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const products = [
+  { product: "banana", price: 3 },
+  { product: "mango", price: 6 },
+  { product: "potato", price: " " },
+  { product: "avocado", price: 8 },
+  { product: "coffee", price: 10 },
+  { product: "tea", price: "" },
+];
+const eachCountry = countries.map((country) =>
+  console.log(country.toUpperCase())
+);
+names.forEach((name) => console.log(name));
+const eachCountr = countries.map((country) => console.log(country.length));
+*/
+
 // ===== While loop = repeat some code
 //        while some condition is true potentially infinite
 /* let userName = "";
